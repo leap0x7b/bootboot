2b8d8674a4b6650679f6b40c08cbd65c764fccd6
Balazs Valkony <pinkfloydhun@gmail.com>
2021-05-06 23:37:53 +0200

    Remove unwanted files of rls

--- mykernel/rust/target/rls/debug/deps/mykernel_rust-da81b4fec964a424.d
+++
@@ -1,6 +0,0 @@
-/home/bambi/git/bootboot/mykernel/rust/target/rls/debug/deps/mykernel_rust-da81b4fec964a424.rmeta: src/main.rs src/bootboot.rs
-
-/home/bambi/git/bootboot/mykernel/rust/target/rls/debug/deps/mykernel_rust-da81b4fec964a424.d: src/main.rs src/bootboot.rs
-
-src/main.rs:
-src/bootboot.rs:
--- mykernel/rust/target/rls/debug/deps/mykernel_rust-f6068bd1b887bf45.d
+++
@@ -1,6 +0,0 @@
-/home/bambi/git/bootboot/mykernel/rust/target/rls/debug/deps/mykernel_rust-f6068bd1b887bf45.rmeta: src/main.rs src/bootboot.rs
-
-/home/bambi/git/bootboot/mykernel/rust/target/rls/debug/deps/mykernel_rust-f6068bd1b887bf45.d: src/main.rs src/bootboot.rs
-
-src/main.rs:
-src/bootboot.rs:
